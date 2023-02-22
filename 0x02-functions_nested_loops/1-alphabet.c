ab87ee71a971cd61acd08dbcdf6d46bb14ecc14d
Antwi Ebenezer <antwinkansah3@gmail.com>
2023-02-22 12:27:26 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
