f2410578e2ace9502197866f5221f22f12a5ca12
Antwi Ebenezer <antwinkansah3@gmail.com>
2023-02-22 19:51:11 -0800

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *jack_bauer - printing every munites of a number starting from
+ *@void: means the parameter has no value
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 2; a++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+	for ((a <= 1 && d < 9) || (a <= 2 && d <= 3))
+	{
+	for (c = 0; c <= 5; C++)
+	{
+	for (d = 0; d <= 9; d++)
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(58);
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	}
+}
