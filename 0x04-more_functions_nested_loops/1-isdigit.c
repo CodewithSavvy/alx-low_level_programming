4d31332921459350cb740255c80650b9a9f9687e
Antwi Ebenezer <antwinkansah3@gmail.com>
2023-02-23 19:32:49 -0800

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if c is  a number through 0 to 9
+ * @c: character to be checked for
+ * Return: 1 otherwise 0
+ */
+
+int _isdigit(int c);
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
