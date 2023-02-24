2d964a7382a2937d59ab03411542aabce2aaffc9
Antwi Ebenezer <antwinkansah3@gmail.com>
2023-02-23 19:43:23 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - prints the multiplication of two numbers
+ * @a:first digit to be multiply
+ * @b: second digit to be mulitiply
+ *Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int multi;
+
+	multi = a * b;
+	_putchar(multi);
+}
