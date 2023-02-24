da6c0da0334aef7a0608c5fb3a21516422aedc81
Antwi Ebenezer <antwinkansah3@gmail.com>
2023-02-23 19:53:22 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - printing digits from 0 to 9
+ *
+ * Retur: ALways 0
+ */
+
+void print_numbers(void)
+{
+	int digit = 0;
+
+	while
+		(digit < 10) {
+			_putchar(digit);
+			digit++;
+	}
+	_putcar('\n');
+	return (0);
+
+}
