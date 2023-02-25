64a98aa9f303eb64118e886d6345151c1d06f2eb
Antwi Ebenezer <antwinkansah3@gmail.com>
2023-02-24 18:03:32 -0800

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - a program that prints a digits from 1 t0 100 on the terminal
+*
+*Return: 0
+*/
+
+int main ()
+{
+int u;
+
+for (u = 1; u <= 100; u++)
+{
+if (u % 3 == 0 && u % 5 != 0)
+{
+	printf(" Fizz");
+}
+else if (u % 5 == 0 && u % 3 != 0)
+{
+	printf(" Buzz");
+}
+else if (u % 5 == 0 && u % 3 == 0)
+{
+	printf(" FizzBuzz");
+}
+else if (u == 1)
+{
+	printf(" %d", u)
+}
+else
+	printf(" %d", u);
+}
+printf("\n");
+}
